47409eea5cbe032b131f5d5a1752dcdb2f9052c7
hek14 <hek14thu@gmail.com>
2024-08-18 23:35:36 +0800

    colemak keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,32 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+---< maps
+local lhs = "neilukj"
+local rhs = "jkluine"
+local modes = {"n","x","o"}
+local opt = {silent=true,noremap=true}
+for i = 1,#lhs do
+  local colemak = lhs:sub(i,i)
+  local qwerty  = rhs:sub(i,i)
+  for _,mode in ipairs(modes) do
+    vim.api.nvim_set_keymap(mode,colemak,qwerty,opt)
+    vim.api.nvim_set_keymap(mode,vim.fn.toupper(colemak),vim.fn.toupper(qwerty),opt)
+    if i < 4 then -- for direction keys
+      vim.api.nvim_set_keymap(mode,"<C-w>" .. colemak,"<C-w>" .. qwerty,opt)
+      vim.api.nvim_set_keymap(mode,"<C-w><C-" .. colemak .. ">","<C-w><C-" .. qwerty .. ">",opt)
+    end
+  end
+end
+vim.api.nvim_set_keymap('n',"<Esc>",':noh<CR>',{silent=true,noremap=true})
+---> maps end
+
+---< options
+vim.o.ignorecase = true
+vim.o.number = true
+vim.o.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.ts = 4
+vim.opt.sw = 4
+vim.opt.autoindent = true
+---> options end
