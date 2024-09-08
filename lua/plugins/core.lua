4411ca3779d25524fb539d6fcb5c010f84d048fe
hek14 <hek14thu@gmail.com>
2024-09-08 16:22:15 +0800

    misc

---
+++ lua/plugins/core.lua
@@ -0,0 +1,11 @@
+return {
+  { "projekt0n/github-nvim-theme" },
+  { "bluz71/vim-moonfly-colors" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "github_dark",
+      colorscheme = "moonfly",
+    },
+  },
+}
